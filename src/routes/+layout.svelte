03988579629292f288a2c889c27fe6efad4ee602
perpetualgrimace <hello@jamesferrell.me>
2022-10-12 17:17:34 -0400

    enables sass & markdown support; scaffolds site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/Services">Services & pricing</a>
+	<a href="/Contact">Contact & booking</a>
+</nav>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,10 @@
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<style>
-	:global(:root) {
-		--color: black;
-		color: var(--color);
+<style lang="scss" global>
+	$thecolor: black;
+
+	:root {
+		color: $thecolor;
 	}
 </style>
