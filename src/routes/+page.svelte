3bfe9217e112f121a5bd2fc424e7209aa8995f98
perpetualgrimace <hello@jamesferrell.me>
2022-10-12 16:50:33 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { globals } from 'svelte/internal';
+</script>
+
+<h1>Hi</h1>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	:global(:root) {
+		--color: black;
+		color: var(--color);
+	}
+</style>
