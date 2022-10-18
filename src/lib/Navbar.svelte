1b90155db28f27ed64cec214b5d8dc74831cd2eb
perpetualgrimace <hello@jamesferrell.me>
2022-10-18 15:11:02 -0400

    adds current page logic to nav

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,21 +1,48 @@
 <script>
+	import { page } from '$app/stores';
+
 	import Logo from '$lib/Logo.svelte';
 </script>
 
 <nav class="navbar">
-	<a class="navbar-link" href=".">
+	<a class="navbar-link" href="." aria-current={!$page.routeId ? 'page' : null}>
 		<Logo />
 	</a>
-	<a class="navbar-link" href="/services">Services & pricing</a>
-	<a class="navbar-link" href="/contact">Contact & booking</a>
+
+	<a
+		class="navbar-link u-font-md"
+		href="/services"
+		aria-current={$page.routeId === 'services' ? 'page' : null}
+	>
+		Services & pricing
+	</a>
+	<a
+		class="navbar-link u-font-md"
+		href="/contact"
+		aria-current={$page.routeId === 'contact' ? 'page' : null}
+	>
+		Contact & booking
+	</a>
 </nav>
 
 <style lang="scss" global>
 	.navbar {
 		display: flex;
+		align-items: center;
 	}
 
 	.navbar-link:first-of-type {
 		margin-right: auto;
+	}
+
+	.navbar-link {
+		padding: 0.75rem;
+		text-decoration: none;
+
+		&:hover,
+		&:focus,
+		&[aria-current] {
+			text-decoration: underline;
+		}
 	}
 </style>
