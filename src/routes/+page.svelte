c38b87dffed0d0cd6c31af87c6e62439854f5dc7
perpetualgrimace <hello@jamesferrell.me>
2022-10-20 16:26:46 -0400

    home page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,19 +25,61 @@
 <main class="wrapper">
 	<Diptych />
 
-	<Artillery heading="heading" subhead="subhead">
-		<p>hi</p>
+	<Artillery heading="Services" subhead="and pricing">
+		<BulletList multiCol fontSize="lg">
+			<li><a href="services#song-production">Song production</a></li>
+			<li><a href="services#audio-engineering">Audio engineering</a></li>
+			<li><a href="services#tracking-editing">Tracking & editing</a></li>
+			<li><a href="services#mixing-mastering">Mixing & mastering</a></li>
+			<li><a href="services#gear-maintenance">Gear maintenance</a></li>
+			<li><a href="services#drum-lessons">Drum lessons</a></li>
+		</BulletList>
 	</Artillery>
 
-	<Artillery heading="heading" subhead="subhead first" subheadAbove showButton>
-		<p>hi</p>
+	<Diptych />
+
+	<Artillery heading="The space" subhead="Small but versatile">
+		<p>
+			The space is a single open 500 sq ft room treated with sound panels and
+			bass traps. It’s optimized for writing, recording, and rehearsing.
+		</p>
+		<p>
+			It has its limitations, but I have workarounds and can quickly reconfigure
+			the inputs for different scenarios. We’ll work together to determine the
+			best approach for your project!
+		</p>
 	</Artillery>
 
-	<BulletList multiCol>
-		<li><a href="">Test</a></li>
-		<li>Test</li>
-		<li>Test</li>
-	</BulletList>
+	<Artillery heading="Instruments" subhead="Source is king">
+		<p>
+			I keep a modest but versatile collection of nice instruments (Gibson,
+			Orange, EVH, DW, Meinl, Yamaha, Ibanez, Schecter, etc). I’m sure you do as
+			well, so we’ll use whatever sounds best!
+		</p>
+	</Artillery>
+
+	<Artillery heading="Microphones" subhead="and audio gear">
+		<p>
+			I have a variety of nice sounding mics for different applications, a
+			Kemper, 16 channels of inputs, and a selection of nice plugins. I employ
+			techniques to emulate the sound of analog in the digital realm — best of
+			both worlds.
+		</p>
+	</Artillery>
+
+	<Artillery
+		heading="James Ferrell"
+		subhead="Studio owner"
+		subheadAbove
+		showButton
+	>
+		<p>
+			I’ve spent most of my life learning, performing, and writing interesting
+			heavy music, and the past 10+ years recording and mixing it. I get the big
+			picture, but also sweat the details.
+		</p>
+		<p>Let’s work together to make your music memorable and massive!</p>
+	</Artillery>
 </main>
 
 <style lang="scss" global>
