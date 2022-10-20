83f79acc7691fc0be5409a4875b393128e32ba29
perpetualgrimace <hello@jamesferrell.me>
2022-10-20 14:34:50 -0400

    adds TextBlock component

---
+++ src/lib/TextBlock.svelte
@@ -0,0 +1,19 @@
+<div class="text-block">
+	<slot />
+</div>
+
+<style lang="scss" global>
+	.text-block {
+		padding: $sp-xl;
+		width: 100%;
+		max-width: 47rem;
+
+		h2 + .u-subhead {
+			margin-top: -$sp-xs;
+		}
+
+		.u-subhead + p {
+			margin-top: $sp-lg;
+		}
+	}
+</style>
