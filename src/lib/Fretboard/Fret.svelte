a092e60dc478386101f8fa089b9e7c21a9fcbd61
perpetualgrimace <hello@jamesferrell.me>
2022-10-20 16:31:36 -0400

    cleans up redundant Fret styles

--- src/lib/Fretboard/Fret.svelte
+++ src/lib/Fretboard/Fret.svelte
@@ -22,21 +22,6 @@
 
 <style lang="scss" global>
 	.fret {
-		overflow: hidden;
-
-		&:not(:last-child) {
-			margin-bottom: 1px;
-		}
-		&:first-of-type {
-			border-top-left-radius: $radius-lg;
-			border-top-right-radius: $radius-lg;
-		}
-		&:last-of-type {
-			border-bottom-left-radius: $radius-lg;
-			border-bottom-right-radius: $radius-lg;
-		}
-	}
-	.fret {
 		padding: $sp-xl $sp-xl $sp-lg $sp-xl;
 		background-color: $dark;
 		z-index: 1;
