b0ec59397d4786c0126650965b783eb937786169
perpetualgrimace <hello@jamesferrell.me>
2022-12-16 16:59:33 -0500

    adds basic audio player + controls

---
+++ src/lib/Player.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+	let player;
+
+	export function togglePlay() {
+		player.paused ? player.play() : player.pause();
+	}
+</script>
+
+<script>
+	export let src;
+</script>
+
+<div class="player">
+	<audio class="player-audio" bind:this={player} {src} controls>
+		<track class="player-track" kind="captions" />
+	</audio>
+</div>
+
+<style global>
+	.player {
+		width: 100%;
+		display: none;
+	}
+</style>
---
+++ src/routes/player/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Hero from '$lib/Hero/index.svelte';
+	import Player, { togglePlay } from '$lib/Player.svelte';
+
+	let tracks = [
+		'https://sveltejs.github.io/assets/music/strauss.mp3',
+		'https://sveltejs.github.io/assets/music/holst.mp3',
+		'https://sveltejs.github.io/assets/music/satie.mp3'
+	];
+
+	const tracksCount = tracks.length;
+
+	$: currSrc = 0;
+
+	function handleNext() {
+		currSrc += 1;
+	}
+	function handlePrev() {
+		currSrc -= 1;
+	}
+</script>
+
+<Hero><h1>player test</h1></Hero>
+
+<div class="wrapper">
+	<button on:click={handlePrev}>⏮</button>
+	<button on:click={togglePlay}>⏯</button>
+	<button on:click={handleNext}>⏭</button>
+
+	<p>current track: <b>{currSrc}</b> ({currSrc + 1} / {tracksCount})</p>
+
+	<Player src={tracks[currSrc]} />
+</div>
