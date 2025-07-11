2d0ef60c41215a2e7667cbba34a53ca2c82f0795
James Ferrell <hello@jamesferrell.me>
2025-07-11 18:49:17 -0400

    repositions first/last releases in ReleaseNav

--- src/lib/ReleasesNav.svelte
+++ src/lib/ReleasesNav.svelte
@@ -5,34 +5,13 @@
 </script>
 
 <TextBlock>
-	<h2 class="u-text-center u-mb-xxs">More releases</h2>
+	<h2 class="u-text-center u-mb-xs">More releases</h2>
 	<p class="u-subhead u-text-center u-mb-md">
-		<a class="u-font-sm" href="./">All releases</a>
+		<a class="u-font-md" href="./">All releases</a>
 	</p>
 
 	<nav class="releases-nav">
 		<ul class="releases-nav-list">
-			{#if !olderRelease}
-				<li class="releases-nav-item">
-					<a class="releases-nav-cover-link" href={`./${newestRelease.slug}`}>
-						<span class="u-visually-hidden">
-							{newestRelease.title} by {newestRelease.artist}
-						</span>
-					</a>
-					<img
-						class="releases-nav-img"
-						src={`/images/releases/${newestRelease.slug}.jpg`}
-						alt=""
-					/>
-					<div class="releases-nav-caption">
-						<div class="u-subhead u-font-sm u-mb-0">Newest release:</div>
-						<div class="releases-nav-heading u-heading u-font-lg u-mb-0">
-							{newestRelease.title} by {newestRelease.artist}
-						</div>
-					</div>
-				</li>
-			{/if}
-
 			{#if newerRelease}
 				<li class="releases-nav-item">
 					<a class="releases-nav-cover-link" href={`./${newerRelease.slug}`}>
@@ -46,9 +25,29 @@
 						alt=""
 					/>
 					<div class="releases-nav-caption">
-						<div class="u-subhead u-font-sm u-mb-0">Newer release:</div>
+						<div class="u-subhead u-font-sm u-mb-0">Newer:</div>
 						<div class="releases-nav-heading u-heading u-font-lg u-mb-0">
 							{newerRelease.title} by {newerRelease.artist}
+						</div>
+					</div>
+				</li>
+			{/if}
+			{#if !newerRelease}
+				<li class="releases-nav-item">
+					<a class="releases-nav-cover-link" href={`./${oldestRelease.slug}`}>
+						<span class="u-visually-hidden">
+							{oldestRelease.title} by {oldestRelease.artist}
+						</span>
+					</a>
+					<img
+						class="releases-nav-img"
+						src={`/images/releases/${oldestRelease.slug}.jpg`}
+						alt=""
+					/>
+					<div class="releases-nav-caption">
+						<div class="u-subhead u-font-sm u-mb-0">Oldest:</div>
+						<div class="releases-nav-heading u-heading u-font-lg u-mb-0">
+							{oldestRelease.title} by {oldestRelease.artist}
 						</div>
 					</div>
 				</li>
@@ -67,30 +66,29 @@
 						alt=""
 					/>
 					<div class="releases-nav-caption">
-						<div class="u-subhead u-font-sm u-mb-0">Older release:</div>
+						<div class="u-subhead u-font-sm u-mb-0">Older:</div>
 						<div class="releases-nav-heading u-heading u-font-lg u-mb-0">
 							{olderRelease.title} by {olderRelease.artist}
 						</div>
 					</div>
 				</li>
 			{/if}
-
-			{#if !newerRelease}
+			{#if !olderRelease}
 				<li class="releases-nav-item">
-					<a class="releases-nav-cover-link" href={`./${oldestRelease.slug}`}>
+					<a class="releases-nav-cover-link" href={`./${newestRelease.slug}`}>
 						<span class="u-visually-hidden">
-							{oldestRelease.title} by {oldestRelease.artist}
+							{newestRelease.title} by {newestRelease.artist}
 						</span>
 					</a>
 					<img
 						class="releases-nav-img"
-						src={`/images/releases/${oldestRelease.slug}.jpg`}
+						src={`/images/releases/${newestRelease.slug}.jpg`}
 						alt=""
 					/>
 					<div class="releases-nav-caption">
-						<div class="u-subhead u-font-sm u-mb-0">Oldest release:</div>
+						<div class="u-subhead u-font-sm u-mb-0">Newest:</div>
 						<div class="releases-nav-heading u-heading u-font-lg u-mb-0">
-							{oldestRelease.title} by {oldestRelease.artist}
+							{newestRelease.title} by {newestRelease.artist}
 						</div>
 					</div>
 				</li>
